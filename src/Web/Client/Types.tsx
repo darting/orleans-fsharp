72fce716a3e50c5ba2e2ec6d4d99001e9eca90c7
Valor <valorzhong@gmail.com>
2018-03-12 02:34:28 +0800

    u

--- src/Web/Client/Types.tsx
+++ src/Web/Client/Types.tsx
@@ -1,9 +1,15 @@
 
 
+export interface User {
+    ID: string,
+    nickname: string,
+}
+
 export interface CounterState {
     counter: number
 }
 
 export interface AppState {
-    
+    player: User,
+    players: User[],
 }
